6ffd37c102d2b3dcc49183aaf4ffc45ed4e3836d
mandeendoh-glitch <mande.endoh@gmail.com>
2026-05-21 22:38:44 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'yashoku-shitsuji-v1';
+const ASSETS = [
+  '/yashoku-shitsuji/',
+  '/yashoku-shitsuji/index.html',
+  '/yashoku-shitsuji/icon-192.png',
+  '/yashoku-shitsuji/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
